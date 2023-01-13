c7aee399c2c72ab43031722eddbf85eb83767401
Huankang Guan <31817414+guanhuankang@users.noreply.github.com>
2023-01-13 21:24:21 +0800

    Update r50frcpn.py
    uncommit uncer=1.0

--- train/net/r50frcpn.py
+++ train/net/r50frcpn.py
@@ -52,7 +52,7 @@ class R50FrcPN(nn.Module):
         if self.training:
             size = y.shape[2::]
             ep_step = 1.0 / kwargs["epoches"]
-            w = [1.0, 1.0, 0.0]
+            w = [1.0, 1.0, 1.0]
             N = len(x) // 2
             alpha_bce = delayWarmUp(step=global_step, period=ep_step * 4, delay=ep_step * 4)
             alpha_other = delayWarmUp(step=global_step, period=ep_step * 4, delay=ep_step * 6)
