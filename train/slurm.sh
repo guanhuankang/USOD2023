fd34158cf1e7d0116fa1f37a1f7a84da7c9364fc
guanhuankang <2659814334@qq.com>
2023-01-14 12:41:55 +0800

    msal gpu_short 12hours

--- train/slurm.sh
+++ train/slurm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --partition=gpu_7d1g
+#SBATCH --partition=gpu_short
 #SBATCH --nodes=1                # 1 computer nodes
 #SBATCH --ntasks-per-node=1      # 1 MPI tasks on EACH NODE
 #SBATCH --cpus-per-task=4        # 4 OpenMP threads on EACH MPI TASK
 #SBATCH --gres=gpu:1             # Using 1 GPU card
 #SBATCH --mem=64GB               # Request 50GB memory
-#SBATCH --time=0-23:59:00        # Time limit day-hrs:min:sec
+#SBATCH --time=0-11:59:00        # Time limit day-hrs:min:sec
 #SBATCH --output=output.log   # Standard output
 #SBATCH --error=error.err    # Standard error log
 
