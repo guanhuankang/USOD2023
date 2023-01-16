7fe6950ea77f9e25bd1e7e43ffe2d16902b68a75
guanhuankang.ghk <2659814334@qq.com>
2023-01-16 15:59:35 +0800

    simter ep30, decay=25, rewrote dataset.py

--- train/dataset.py
+++ train/dataset.py
@@ -1,83 +1,14 @@
 #!/usr/bin/python3
 #coding=utf-8
 
-import os, json
-import cv2
+import os
 import torch
+import torch.nn.functional as F
 import numpy as np
+from PIL import Image
+from torchvision import transforms as pth_transforms
 from torch.utils.data import Dataset
-import albumentations as DA
-from common import *
-
-########################### Data Augmentation ###########################
-class Normalize(object):
-    def __init__(self, mean, std):
-        self.mean = mean 
-        self.std  = std
-    def __getstate__(self): return self.__dict__
-    def __setstate__(self, d): self.__dict__.update(d)
-    def __call__(self, images, maps=[]):
-        images = [(img - self.mean)/self.std for img in images]
-        if len(maps) <= 0:
-            return images
-        maps = [m/255 for m in maps]
-        return tuple(images), tuple(maps)
-
-class RandomCrop(object):
-    def __getstate__(self): return self.__dict__
-    def __setstate__(self, d): self.__dict__.update(d)
-    def __call__(self, images, maps=[]):
-        image = images[0]
-        H,W,_   = image.shape
-        randw   = np.random.randint(W/8)
-        randh   = np.random.randint(H/8)
-        offseth = 0 if randh == 0 else np.random.randint(randh)
-        offsetw = 0 if randw == 0 else np.random.randint(randw)
-        p0, p1, p2, p3 = offseth, H+offseth-randh, offsetw, W+offsetw-randw
-        images = [image[p0:p1,p2:p3, :] for image in images]
-        if len(maps) <= 0:
-            return images
-        maps = [m[p0:p1,p2:p3] for m in maps]
-        return tuple(images), tuple(maps)
-
-class RandomFlip(object):
-    def __getstate__(self): return self.__dict__
-    def __setstate__(self, d): self.__dict__.update(d)
-    def __call__(self, images, maps=[]):
-        if np.random.randint(2)==0:
-            images = [image[:,::-1,:].copy() for image in images]
-            if len(maps) <= 0:
-                return images
-            maps = [m[:, ::-1].copy() for m in maps]
-            return tuple(images), tuple(maps)
-        else:
-            if len(maps) <= 0:
-                return images
-            return tuple(images), tuple(maps)
-
-class Resize(object):
-    def __init__(self, H, W):
-        self.H = H
-        self.W = W
-    def __getstate__(self): return self.__dict__
-    def __setstate__(self, d): self.__dict__.update(d)
-    def __call__(self, images, maps=[]):
-        images = [ cv2.resize(image, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR) for image in images]
-        if len(maps) <= 0:
-            return images
-        maps = [cv2.resize( m, dsize=(self.W, self.H), interpolation=cv2.INTER_LINEAR) for m in maps]
-        return tuple(images), tuple(maps)
-
-class ToTensor(object):
-    def __getstate__(self): return self.__dict__
-    def __setstate__(self, d): self.__dict__.update(d)
-    def __call__(self, image, *maps):
-        image = torch.from_numpy(image)
-        image = image.permute(2, 0, 1)
-        if len(maps) <= 0:
-            return image
-        maps = [image] + [ torch.from_numpy(m) for m in maps]
-        return tuple(maps)
+from common import loadJson, nestedNameSpaceFromDict
 
 ########################### Dataset Class ###########################
 class Data(Dataset):
@@ -89,47 +20,47 @@ class Data(Dataset):
         print("datasetCfg", datasetCfg)
 
         self.cfg        = cfg
-        self.normalize  = Normalize(mean=cfg.mean, std=cfg.std)
-        self.randomcrop = RandomCrop()
-        self.randomflip = RandomFlip()
-        self.resize     = Resize(*cfg.size)
-        self.totensor   = ToTensor()
         self.samples = [ x[0:-4] for x in os.listdir(datasetCfg.image.path) if x.endswith(datasetCfg.image.suffix) ]
         self.datasetCfg = datasetCfg
     def __getstate__(self): return self.__dict__
+
     def __setstate__(self, d): self.__dict__.update(d)
+
     def __getitem__(self, idx):
-        transform = DA.Compose(
+        cfg = self.cfg
+
+        share_transform = pth_transforms.Compose([
+            pth_transforms.Resize(cfg.size),
+            pth_transforms.ToTensor(),
+        ])
+        image_norm = pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        pixel_transform = pth_transforms.Compose(
             [
-                DA.ColorJitter(p=1.0),
-                DA.CLAHE(),
-                DA.GaussNoise(),
-                DA.GaussNoise(),
-                DA.RandomBrightnessContrast(p=0.5),
-                DA.RGBShift()
+                pth_transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+                pth_transforms.GaussianBlur(5)
             ]
         )
+        layout_transform = pth_transforms.Compose([
+            pth_transforms.RandomResizedCrop(size=cfg.size, scale=(0.49, 1.0)),
+            pth_transforms.RandomHorizontalFlip()
+        ])
 
         name  = self.samples[idx]
-        image = cv2.imread(os.path.join(self.datasetCfg.image.path, name+self.datasetCfg.image.suffix))[:,:,::-1].astype(np.uint8) ## RGB
+        image = Image.open(os.path.join(self.datasetCfg.image.path, name+self.datasetCfg.image.suffix))
+        mask =  Image.open(os.path.join(self.datasetCfg.mask.path, name+self.datasetCfg.mask.suffix)).convert("L")
 
         if self.cfg.mode=='train':
-            mask = cv2.imread(os.path.join(self.datasetCfg.mask.path, name + self.datasetCfg.mask.suffix), 0).astype(
-                np.float32)
-            # sal  = cv2.imread(os.path.join(salCfg.path, name+salCfg.suffix), 0).astype(np.float32)
-
-            images, masks = self.normalize([image.astype(np.float32), transform(image=image)["image"].astype(np.float32)], [mask])
-            images, masks = self.randomcrop(images, masks)
-            images, masks = self.randomflip(images, masks)
-            return images[0], images[1], masks[0]
+            i1 = share_transform(image)
+            i2 = share_transform(pixel_transform(image))
+            mak = share_transform(mask)
+            combine = layout_transform(torch.cat([i1,i2,mak], dim=0))
+            i1, i2, mak = image_norm(combine[0:3,:,:]), image_norm(combine[3:6,:,:]), combine[6:7,:,:].gt(0.5).float()
+            return i1, i2, mak
         else:
-            shape = image.shape[:2]
-            uint8_img = image.copy()
-
-            images = self.normalize([image.astype(np.float32)])
-            image = self.resize(images)[0]
-            image = self.totensor(image)
-            return image, uint8_img, shape, name
+            i1 = image_norm(share_transform(image))
+            uimg = np.array(image).astype(np.uint8)
+            shape = uimg.shape[:2]
+            return i1, uimg, shape, name
 
     def __len__(self):
         return len(self.samples)
@@ -140,11 +71,8 @@ class Data(Dataset):
 
     def collate(self, batch):
         size = [224, 256, 288, 320, 352][np.random.randint(0, 5)]
-        image0, image1, mask = [list(item) for item in zip(*batch)]
-        for i in range(len(batch)):
-            image0[i] = cv2.resize(image0[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR)
-            image1[i] = cv2.resize(image1[i], dsize=(size, size), interpolation=cv2.INTER_LINEAR)
-            mask[i]  = cv2.resize(mask[i],  dsize=(size, size), interpolation=cv2.INTER_LINEAR)
-        image  = torch.from_numpy(np.stack(image0 + image1, axis=0)).permute(0,3,1,2)
-        mask   = torch.from_numpy(np.stack(mask + mask, axis=0)).unsqueeze(1)
-        return image, mask
+        i1, i2, mak = [list(item) for item in zip(*batch)]
+        i1 = F.interpolate(torch.stack(i1, dim=0), size=size, mode="bilinear")
+        i2 = F.interpolate(torch.stack(i2, dim=0), size=size, mode="bilinear")
+        mak = F.interpolate(torch.stack(mak, dim=0), size=size, mode="bilinear").gt(0.5).float()
+        return torch.cat([i1, i2], dim=0), torch.cat([mak, mak], dim=0)
