0f989d695bdf738cf8cde875841eb2f95a1bd13e
guanhuankang <2659814334@qq.com>
2023-01-25 10:59:40 +0800

    CJRBCS init

--- train/dataset.py
+++ train/dataset.py
@@ -102,9 +102,6 @@ class Data(Dataset):
         transform = DA.Compose(
             [
                 DA.ColorJitter(p=1.0),
-                DA.CLAHE(),
-                DA.GaussNoise(),
-                DA.GaussNoise(),
                 DA.RandomBrightnessContrast(p=0.5),
                 DA.RGBShift()
             ]
