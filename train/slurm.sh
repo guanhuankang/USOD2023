e0b16e7d8630795a85d6134866508b63ad0e9693
guanhuankang <2659814334@qq.com>
2023-01-28 19:55:05 +0800

    ftdice 7d1g

--- train/slurm.sh
+++ train/slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --partition=gpu_short
+#SBATCH --partition=gpu_7d1g
 #SBATCH --nodes=1                # 1 computer nodes
 #SBATCH --ntasks-per-node=1      # 1 MPI tasks on EACH NODE
 #SBATCH --cpus-per-task=4        # 4 OpenMP threads on EACH MPI TASK
