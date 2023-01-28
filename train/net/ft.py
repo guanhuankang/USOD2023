e84e1ab27ff6ec512e698365c0dfd04bc3012458
guanhuankang <2659814334@qq.com>
2023-01-28 19:45:35 +0800

    ftiou init

--- train/net/ft.py
+++ train/net/ft.py
@@ -23,7 +23,12 @@ def uphw(x, size):
     return F.interpolate(x, size=size, mode="bilinear")
 
 def headLoss(y, p):
-    return F.binary_cross_entropy_with_logits(p, y)
+    p = torch.sigmoid(p)
+    y = y.gt(0.5).float()
+    inter = (p * y).mean(dim=[1,2,3])
+    union = (p + y).mean(dim=[1,2,3]) - inter
+    iou = (inter + 1e-6) / (union + 1e-6)
+    return 1.0 - iou
 
 class FT(nn.Module):
     def __init__(self, cfg):
