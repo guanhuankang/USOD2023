6408e4a0dc9cc45afbdc47284ea608d8d45a1ef1
guanhuankang.ghk <2659814334@qq.com>
2023-02-04 15:36:39 +0800

    ablation onlybce init

--- train/net/r50frcpn.py
+++ train/net/r50frcpn.py
@@ -52,21 +52,13 @@ class R50FrcPN(nn.Module):
         if self.training:
             size = y.shape[2::]
             ep_step = 1.0 / kwargs["epoches"]
-            w = [1.0, 0.0, 0.0]
-            N = len(x) // 2
             alpha_bce = delayWarmUp(step=global_step, period=ep_step * 4, delay=ep_step * 4)
-            alpha_other = delayWarmUp(step=global_step, period=ep_step * 4, delay=ep_step * 6)
             loss_dict = {"clloss": loss.item()}
 
             sal_cues = self.crf(uphw(minMaxNorm(x),size=size), minMaxNorm(uphw(attn.detach(), size=size)), iters=3).gt(0.5).float() ## stop gradient
-            if alpha_bce>1e-3 and alpha_bce<0.999:
+            if alpha_bce>1e-3:
                 bceloss = F.binary_cross_entropy_with_logits(y, sal_cues); loss_dict.update({"bce_loss": bceloss.item()})
                 loss += bceloss
-            if alpha_other>1e-3:
-                lwtloss = self.lwt(torch.sigmoid(y), minMaxNorm(x), margin=0.5); loss_dict.update({"lwt_loss": lwtloss.item()})
-                consloss = F.l1_loss(torch.sigmoid(y[0:N]), torch.sigmoid(y[N::])); loss_dict.update({"cons_loss": consloss.item()})
-                uncerloss = 0.5 - torch.abs(torch.sigmoid(y) - 0.5).mean(); loss_dict.update({"uncertain_loss": uncerloss.item()})
-                loss += lwtloss * w[0] + consloss * w[1] + uncerloss * w[2]
 
             loss_dict.update({"tot_loss": loss.item()})
             if "sw" in kwargs:
