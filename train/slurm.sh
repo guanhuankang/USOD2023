d0bbbfa89b94751e24c37edd5b0a270dfa9b52ae
guanhuankang <2659814334@qq.com>
2023-02-07 14:24:42 +0800

    detmask: use gt to supervised s2

--- train/slurm.sh
+++ train/slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4        # 4 OpenMP threads on EACH MPI TASK
 #SBATCH --gres=gpu:1             # Using 1 GPU card
 #SBATCH --mem=64GB               # Request 50GB memory
-#SBATCH --time=2-11:59:00        # Time limit day-hrs:min:sec
+#SBATCH --time=0-23:59:00        # Time limit day-hrs:min:sec
 #SBATCH --output=output.log   # Standard output
 #SBATCH --error=error.err    # Standard error log
 
